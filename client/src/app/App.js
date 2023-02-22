e63fa4a01bc0bc60f62088c7e5a03a75ccb3e8cd
fil <philippe.teisseyre@laposte.net>
2023-02-22 12:37:01 +0100

    layout added

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -4,18 +4,27 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from './components/Home';
 import Chat from './components/Chat/Chat';
+import "./components/Layout";
 import "./components/Styles/App.css";
+
+function LayoutComponent({ component: Component, layout: Layout }) {
+  return (
+    <Layout>
+      <Component />
+    </Layout>
+  );
+}
 
 function App() {
   return (
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Register' element={Register} />
-          <Route path='/Chat' element={<Chat />} />
-        </Routes>
-      </div>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<LayoutComponent component={Home} layout={Layout} />} />
+        <Route path='/Login' element={<LayoutComponent component={Login} layout={Layout} />} />
+        <Route path='/Register' element={<LayoutComponent component={Register} layout={Layout} />} />
+        <Route path='/Chat' element={<LayoutComponent component={Chat} layout={Layout} />} />
+      </Routes>
+    </div>
   );
 }
 
